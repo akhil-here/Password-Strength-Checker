7136f8befe2ee3c24b27579300e800e3b3223f70
Akhil Namboodiri <akhil.namboodiri@somaiya.edu>
2021-06-10 00:48:42 +0530

    check strength function added, html page modified

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def display():
     return render_template('index.html')
 
 
-@app.route("/intro")
-def intro():
-    return 'Hello Akhil!!!'
+@app.route("/check", methods=['POST'])
+def check_strength(strength=strength):
+    if request.method == 'POST':
+        password = request.form['pwd']
+        return render_template('index.html', strength="Strong!!")
+    else:
+        return render_template('index.html', strength=" ")
 
 
 if __name__ == '__main__':
